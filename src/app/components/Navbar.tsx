12bfca4e8dc1579cf68c9314b0f6da4bf33c7ece
Carlos Adames <92492690+KamajiRd@users.noreply.github.com>
2023-07-19 21:33:16 -0400

    Created NavBar TODO: Finish Hamburger Menu

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,8 @@
 "use client"
 import React, {useState} from "react"
 import Link from "next/link"
+import { useTheme } from "next-themes"
+import Hamburger from "./hamburger"
 
 interface NavItem {
     label: string
@@ -27,9 +29,31 @@ const NAV_ITEMS: Array<NavItem> = [
 ]
 
 const Navbar = () => {
+    const {systemTheme, theme, setTheme} = useTheme()
+    const currentTheme = theme === "system" ? systemTheme : theme
+    const [navbar, setNavbar] = useState(false)
+
     return (
-        <nav>Hello World</nav>
+        <header className="m-0 p-0">
+            <nav className="justify-between md:items-center md:flex pt-15 mx-24 sm:flex">
+                <div>
+                    <h2 className="font-bold text-lg">CA</h2>
+                </div>
+                <div className=" md:flex md:gap-4 cursor-pointer sm:hidden ">
+                    
+                    {NAV_ITEMS.map((item,idx) => { 
+                    return <a className="hover:text-emerald-400" key={idx}>{item.label}</a>
+                    })}
+                </div>
+                <div className="md:hidden">
+                    <Hamburger />
+                </div>
+            </nav>
+        </header>
     )
 }
 
 export default Navbar
+
+
+
