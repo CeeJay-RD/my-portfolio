64c261c810c7c98a53cebad604c61f86b070f343
CeeJay-RD <adamescarlos2808@gmail.com>
2023-07-21 13:44:35 -0400

    Added text to a container, and changed font size to clamp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,10 @@ export default function Home() {
           <div className=' bg-white rounded-full w-40 h-40 p-2 grid items-center'>
             <img src="https://raw.githubusercontent.com/CeeJay-RD/my-portfolio/792ab4a4ddabd946be033d6cf295cafd7461c53d/public/thumbs-up-memoji.png?token=GHSAT0AAAAAACEAHOPTUXTIEF4P6AOF5H4GZF2UEZA" alt="memoji-thumbsup" className='w-40' />
           </div>
-          <h3 className='text-5xl font-bold	 mt-4'>Hey, I'm <span className=' text-emerald-200'>Carlos</span></h3>
-          <h1 className='max-w-5xl text-6xl text-center font-bold'> <span className='text-slate-400'>&lt;</span><span className='text-emerald-400'>h1</span><span className='text-slate-400'>&gt;</span> Bringing joy to users through web development<span className='text-slate-400'>&lt;/</span><span className='text-emerald-400'>h1</span><span className='text-slate-400'>&gt;</span> </h1>
+          <div>
+            <h3 className='text-h3 font-bold text-center	mt-4'>Hey, I'm <span className=' text-emerald-200'>Carlos</span></h3>
+            <h1 className='max-w-5xl text-h1 text-center font-bold'> <span className='text-slate-400'>&lt;</span><span className='text-emerald-400'>h1</span><span className='text-slate-400'>&gt;</span> Bringing joy to users through web development<span className='text-slate-400'>&lt;/</span><span className='text-emerald-400'>h1</span><span className='text-slate-400'>&gt;</span> </h1>
+          </div>
         </div>
       </section>
     </main>
