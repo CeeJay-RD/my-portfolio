fca89fb729bc488509ab6e4d4f6b327193ab6088
Carlos Adames <92492690+KamajiRd@users.noreply.github.com>
2023-07-20 22:23:45 -0400

    added dark mode gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,13 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-dark':
+          'linear-gradient(358deg, #2B9B73 0%, rgba(15, 23, 42, 0.90) 97.40%)'
       },
       padding: {
         '15': '3.75rem'
       },
+      
     },
   },
   plugins: [],
