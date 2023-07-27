52c70891a45d4bec18415c91eb9f456b40fa1407
Carlos Adames <92492690+KamajiRd@users.noreply.github.com>
2023-07-26 22:00:38 -0400

    Created a separate file for the project data

---
+++ src/app/components/data/PROJECTS_DATA.tsx
@@ -0,0 +1,21 @@
+export const PROJECT_ITEMS = [
+    {
+        title: "Rock, Paper, Scissors",
+        thumbnail: "#",
+        githubLink: "#",
+        deployLink: "#"
+    },
+    {
+        title: "MindNet",
+        thumbnail: "#",
+        githubLink: "#",
+        deployLink: "#"
+    },
+    {
+        title: "Etch-a-Sketch",
+        thumbnail: "#",
+        githubLink: "#",
+        deployLink: "#"
+    },
+
+]
