90e7a2d6389283954b6c305c27a2175eecbea9f8
Carlos Adames <92492690+KamajiRd@users.noreply.github.com>
2023-08-01 21:39:07 -0400

    Adding responsive flex layout

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -6,7 +6,7 @@ import { PROJECT_ITEMS } from "./data/PROJECTS_DATA";
 const Carousel = () => {
   return (
     <>
-      <div className="flex mt-5 gap-10">
+      <div className="flex flex-col md:flex md:flex-row mt-5 gap-10">
         {PROJECT_ITEMS.map((project) => {
           return <ProjectCard {...project}/>;
         })}
