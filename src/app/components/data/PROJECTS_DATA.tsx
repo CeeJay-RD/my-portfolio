d6947e3b9a5891047bf604faef354fa182dd88fe
KamajiRD <adamescarlos2808@gmail.com>
2023-08-03 18:25:05 -0400

    Adding Project Data and Links

--- src/app/components/data/PROJECTS_DATA.tsx
+++ src/app/components/data/PROJECTS_DATA.tsx
@@ -1,21 +1,21 @@
 export const PROJECT_ITEMS = [
     {
         title: "Rock, Paper, Scissors",
-        thumbnail: "#",
-        githubLink: "#",
-        deployLink: "#"
+        thumbnail: "../../../../public/rock-paper-scissors-thumbnail.png",
+        githubLink: "https://github.com/CeeJay-RD/Rock-Paper-Scissors",
+        deployLink: "https://ceejay-rd.github.io/Rock-Paper-Scissors/"
     },
     {
         title: "MindNet",
         thumbnail: "#",
-        githubLink: "#",
+        githubLink: "https://github.com/CeeJay-RD/Final-Project-Mindnet",
         deployLink: "#"
     },
     {
         title: "Etch-a-Sketch",
-        thumbnail: "#",
-        githubLink: "#",
-        deployLink: "#"
+        thumbnail: "https://camo.githubusercontent.com/ead024e2655aee6989d3356a493bf1a1bd3c7fbd6a1807d89b3be4801ad6be2c/68747470733a2f2f73332e67696679752e636f6d2f696d616765732f457463682d612d536b657463682e676966",
+        githubLink: "https://github.com/CeeJay-RD/etch-a-sketch",
+        deployLink: "https://ceejay-rd.github.io/etch-a-sketch/"
     },
 
 ]
