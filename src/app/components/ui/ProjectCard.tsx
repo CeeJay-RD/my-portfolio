e4ac17f4162be83f03d407171fb5b1b91147d2c5
Carlos Adames <92492690+KamajiRd@users.noreply.github.com>
2023-08-09 22:37:17 -0400

    Added fixed width to Project Thumbnails 📏

--- src/app/components/ui/ProjectCard.tsx
+++ src/app/components/ui/ProjectCard.tsx
@@ -11,7 +11,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ title, thumbnail, githubLink,
   return (
     <div className="flex flex-col bg-slate-600 rounded-md p-7">
       <p className=" text-p font-bold">{title}</p>
-      <img src={thumbnail} alt={title} />
+      <img src={thumbnail} alt={title} className=" w-[20rem] h-[20rem]"/>
       <div className="flex">
         <Download fileSrc={githubLink} text="Github" />
         <Download fileSrc={deployLink} text="Deploy" />
