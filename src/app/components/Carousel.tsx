c73cf1312bdc4194526b28a55a1ed94ca0a32814
Carlos Adames <92492690+KamajiRd@users.noreply.github.com>
2023-08-09 21:29:30 -0400

    Added ID to resolve compiler error

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -1,14 +1,13 @@
 import ProjectCard from "./ui/ProjectCard";
-
 import { PROJECT_ITEMS } from "./data/PROJECTS_DATA";
 
 
 const Carousel = () => {
   return (
     <>
-🛠      <div className="flex flex-col md:flex md:flex-row mt-5 gap-10 overflow-scroll pb-14">
-        {PROJECT_ITEMS.map((project) => {
-          return <ProjectCard {...project}/>;
+        <div className="flex flex-col md:flex md:flex-row mt-5 gap-10 overflow-scroll pb-14">
+        {PROJECT_ITEMS.map((project, idx) => {
+          return <ProjectCard {...project} key={idx}/>;
         })}
       </div>
     </>
